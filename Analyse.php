70c85b1b7507cd6147b8107b0c5dfe65c6b036bb
Ansh Kapoor <ansh605@gmail.com>
2018-08-13 16:21:16 +0530

    Added function to calculate grade point per subject score

--- Analyse.php
+++ Analyse.php
@@ -2,12 +2,67 @@
 class student{
 $subjects = array("Maths","TOC","COA","DBMS","OOPS","CS");
 $name;
-$gpa;
+$gpa=array();
+$cgpa;
+$marks=array();
+$length= count($marks);
+function calc_GPA(){
+for($i=0;$i<$length;$i++){
+if($marks[$i]>=90 && $marks[$i]<=100)
+array_push($gpa,10);
 
+else if($marks[$i]>=75 && $marks[$i]<=89)
+array_push($gpa,9);
+
+else if($marks[$i]>=65 && $marks[$i]<=74)
+array_push($gpa,8);
+
+else if($marks[$i]>=55 && $marks[$i]<=64)
+array_push($gpa,7);
+
+else if($marks[$i]>=50 && $marks[$i]<=54)
+array_push($gpa,6);
+
+else if($marks[$i]>=45 && $marks[$i]<=49)
+array_push($gpa,5);
+
+else if($marks[$i]>=40 && $marks[$i]<=44)
+array_push($gpa,4);
+
+else 
+array_push($gpa,0);
+
+
+
+
+}
+
+
+
+
+} 
 
 
 }
 
 $Maths = $_GET["sub1"];
 $TOC =$_GET["sub2"];
-$COA= $_GET["su
+$COA= $_GET["sub3"];
+$DBMS   =$_GET["sub4"];
+$OOP  =$_GET["sub5"];
+$CS   =$_GET["sub6"];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
