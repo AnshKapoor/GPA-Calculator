e147a41a1d3de3d97b21a8adcd1c018e9e906088
Ansh Kapoor <ansh605@gmail.com>
2018-08-13 15:52:26 +0530

    Added student class

--- Analyse.php
+++ Analyse.php
@@ -1,24 +1,13 @@
 <?php
+class student{
+$subjects = array("Maths","TOC","COA","DBMS","OOPS","CS");
+$name;
+$gpa;
 
+
+
+}
 
 $Maths = $_GET["sub1"];
 $TOC =$_GET["sub2"];
-$COA= $_GET["sub3"];
-$DBMS   =$_GET["sub4"];
-$OOP  =$_GET["sub5"];
-$CS   =$_GET["sub6"];
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-?>
+$COA= $_GET["su
