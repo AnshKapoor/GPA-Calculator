cefd2f246ea976e86dd3269b0371d2adbcb0890f
Ansh Kapoor <ansh605@gmail.com>
2018-08-13 15:32:19 +0530

    Created php variables for storing marks

---
+++ Analyse.php
@@ -0,0 +1,24 @@
+<?php
+
+
+$Maths = $_GET["sub1"];
+$TOC =$_GET["sub2"];
+$COA= $_GET["sub3"];
+$DBMS   =$_GET["sub4"];
+$OOP  =$_GET["sub5"];
+$CS   =$_GET["sub6"];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
