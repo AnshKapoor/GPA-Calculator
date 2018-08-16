c27748ce56874c509a686a8dc442c72e3b0ee5a8
Ansh Kapoor <ansh605@gmail.com>
2018-08-16 08:42:46 +0530

    Added credits upto 4th semester

--- Calculate.js
+++ Calculate.js
@@ -1,8 +1,33 @@
 var subjects_credits = { "semester4":{"Maths":4,
-"TOC":4,
+{"TOC":4,
 "COA":4,
 "DBMS":4,
 "OOP":3,
 "CS":4
+},
+"semester3":{ "Maths-3":4,
+"FCS":4,
+"STLD":4,
+"CnS":4,
+"DS":4,
+"CGMM":4
+ },
+"semester2":{"Maths-2":4,
+"Physics-2":3,
+"ED":3,
+"IP":3,
+"EM":3,
+"Communication":3,
+"EVS":3
+ },
+"semester1":{
+"Maths-1":4,
+"Physics-1":3,
+"MP":3,
+"ET":3,
+"HV":1,
+"FCS":2,
+"Chemistry":3
+
 }
 }
