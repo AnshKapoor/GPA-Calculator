d59b13ccb87f4cdcd14f049db229c699caa971f0
AnshKapoor <ansh605@gmail.com>
2018-08-18 23:49:59 +0530

    Removed double quotes from all object properties

--- Calculate.js
+++ Calculate.js
@@ -1,40 +1,42 @@
-var subjects_credits = { "semester4":{"Maths":4,
-"TOC":4,
-"COA":4,
-"DBMS":4,
-"OOP":3,
-"CS":4
+var subjects_credits = { semester4:{Maths:4,
+TOC:4,
+COA:4,
+DBMS:4,
+OOP:3,
+CS:4
 },
-"semester3":{ "Maths-3":4,
-"FCS":4,
-"STLD":4,
-"CnS":4,
-"DS":4,
-"CGMM":4
+semester3:{Maths_3:4,
+FCS:4,
+STLD:4,
+CnS:4,
+DS:4,
+CGMM:4
  },
-"semester2":{"Maths-2":4,
-"Physics-2":3,
-"ED":3,
-"IP":3,
-"EM":3,
-"Communication":3,
-"EVS":3
+semester2:{Maths_2:4,
+Physics_2:3,
+ED:3,
+IP:3,
+EM:3,
+Communication:3,
+EVS:3
  },
-"semester1":{
-"Maths-1":4,
-"Physics-1":3,
-"MP":3,
-"ET":3,
-"HV":1,
-"FCS":2,
-"Chemistry":3
+semester1:{
+Maths_1:4,
+Physics_1:3,
+MP:3,
+ET:3,
+HV:1,
+FCS:2,
+Chemistry:3
 
 }
 }
 function printvalues(){
 var values = $("input[name='sub[]']")
               .map(function(){return $(this).val();}).get();
-const myobj = {"semester4"};
-alert(myobj);
+var pickedsemester = function({a}){
 
+return {a};
+}
+console.log(pickedsemester(subject_credits));
 }
