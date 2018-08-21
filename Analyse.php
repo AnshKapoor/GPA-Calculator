b9ff090a7a54406c55772887529d5d313d4bf6ae
AnshKapoor <ansh605@gmail.com>
2018-08-21 20:18:21 +0530

    Created seprate file for plotting data in charts

--- Analyse.php
+++ Analyse.php
@@ -1,14 +1,27 @@
 <?php
-
-
-
-
 $Maths = $_GET["sub1"];
 $TOC =$_GET["sub2"];
 $COA= $_GET["sub3"];
 $DBMS   =$_GET["sub4"];
 $OOP  =$_GET["sub5"];
 $CS   =$_GET["sub6"];
-
-
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>GPA Calculator</title>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
+<link rel="stylesheet" type="text/css" href="styles.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
+<script 
+src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+ <script src="Calculate.js"></script> 
+<script src="chartsjs.js"></script> 
+
+</head>
+<body>
+<canvas id="myChart"> </canvas>
+
+
+</body>
+</html>
