bb0c41b0c53d47748a1fb260ae9e0fb19d5f3c19
Ansh Kapoor <32305554+AnshKapoor@users.noreply.github.com>
2018-09-18 19:43:14 +0530

    Removing node js server
    The project will continue in php as backend and not node js

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 
-var express = require('express');
-var app = express();
-var path = require('path');
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index1.html'));
+//var express = require('express');
+//var app = express();
+//var path = require('path');
+//app.get('/', function(req, res) {
+  //  res.sendFile(path.join(__dirname + '/index1.html'));
 
-});
+//});
 
-app.listen(8080);
-app.use(express.static('public'));
+//app.listen(8080);
+//app.use(express.static('public'));
