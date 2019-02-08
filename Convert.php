556d4bfcf78fbc9778b3620f393a7fda54737fe5
AnshKapoor <ansh605@gmail.com>
2019-02-09 01:51:48 +0530

    Adding regular expressions

--- Convert.php
+++ Convert.php
@@ -1,22 +1,24 @@
 <?php
 require_once('/convertapi-php-master/lib/ConvertApi/autoload.php');
 use \ConvertApi\ConvertApi;
-ConvertApi::setApiSecret('NB8EreFDNukmE3Gd');
-$result = ConvertApi::convert('txt', [
-    'File' => 'http://ggsipu.ac.in/ExamResults/2018/010818/btech/2015%20Batch%20Onwards/6TH%20SEMESTER/031_IT_6thSEM_2015%20Batch%20Onwards.pdf',
-], 'pdf'
-);
-$result->saveFiles('convertedIPU');
+//ConvertApi::setApiSecret('NB8EreFDNukmE3Gd');
+//$result = ConvertApi::convert('txt', [
+  //  'File' => 'http://ggsipu.ac.in/ExamResults/2018/010818/btech/2015%20Batch%20Onwards/6TH%20SEMESTER/031_IT_6thSEM_2015%20Batch%20Onwards.pdf',
+//], 'pdf'
+//);
+//$result->saveFiles('convertedIPU');
 $Vdata = file_get_contents("convertedIPU");
-$re = '/(\d+)\([A-Z]\+?\)/';
-preg_match_all($re, $Vdata, $matches);
-
-//$values = var_dump(array_map('intval',$matches[0]));
-$values = array_map('intval',$matches[0]);
-foreach($values as $value){
-    echo "<p> $value </p>";
-}
-
-
-
+$your_array = explode("\n", $Vdata);
 ?>
+<script>
+var abc = <?php echo json_encode($Vdata) ?>;
+
+var regexForStudents = /Result of Programme Code:([^]*?)\f/g;
+var regexForSubjects = /S\.No\. Paper([^]*?)RESULT TAB/g;
+var students = abc.match(regexForStudents);
+var subjects = abc.match(regexForSubjects);
+document.write(subjects);
+
+
+
+</script>
